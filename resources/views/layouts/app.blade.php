f3955a535f7a51d87c44b9b5e2f35d3af8750b05
ahmdhzq <haziexahmad18@gmail.com>
2025-07-24 23:35:44 +0700

    Fix debug css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'CodeLearn')</title>
     <!-- VITE ASSET -->
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @vite(['resources/js/app.js'])
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
