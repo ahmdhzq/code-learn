5f86d728e96c854e94354b88d30f89d22244e7de
ahmdhzq <haziexahmad18@gmail.com>
2025-07-28 06:25:35 +0700

    fix: database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,6 +6,20 @@ class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
+        $adminRole = \Spatie\Permission\Models\Role::firstOrCreate(['name' => 'admin']);
+
+        $adminUser = \App\Models\User::firstOrCreate(
+            ['email' => 'admin@codelearn.com'],
+            [
+                'name' => 'Admin User',
+                'password' => bcrypt('password'),
+            ]
+        );
+
+        if (!$adminUser->hasRole('admin')) {
+            $adminUser->assignRole($adminRole);
+        }
+
         $this->call([
             UserSeeder::class,
             TrackSeeder::class,
